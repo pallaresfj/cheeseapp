2c7377ae8e263e669f532d6be1377351fdbbf795
pallaresfj <pallaresfj@yahoo.es>
2025-05-31 21:45:12 -0500

    Add widget balance semanal

---
+++ app/Filament/Widgets/StatsWeeklyBalance.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\Setting;
+use App\Models\WeeklyBalance;
+use Carbon\Carbon;
+
+class StatsWeeklyBalance extends BaseWidget
+{
+    protected int | string | array $columnSpan = 'full';
+    protected static ?int $sort = 6;
+
+    protected function getStats(): array
+    {
+        $lastDate = WeeklyBalance::latest('date')->value('date');
+        $startDate = Carbon::parse($lastDate)->subDays(6)->startOfDay();
+        $leyendaFechas = $startDate->format('d/m/Y') . ' - ' . Carbon::parse($lastDate)->endOfDay()->format('d/m/Y');
+        $ingresos = WeeklyBalance::whereBetween('date', [
+            $startDate,
+            Carbon::parse($lastDate)->endOfDay(),
+        ])->sum('incomes');
+        $egresos = WeeklyBalance::whereBetween('date', [
+            $startDate,
+            Carbon::parse($lastDate)->endOfDay(),
+        ])->sum('expenses');
+        // $productividad = Setting::where('key', 'sistema.productividad')->value('value') ?? 1;
+        // $kilosEsperados = $totalLitros * $productividad;
+        $balance = $ingresos - $egresos;
+        return [
+            Stat::make('Ingresos', '$' . number_format($ingresos, 1, ',', '.'))
+                ->description($leyendaFechas)
+                ->color('success')
+                ->icon('heroicon-o-arrow-down-on-square'),
+            Stat::make('Egresos', '$' . number_format($egresos, 1, ',', '.'))
+                ->description($leyendaFechas)
+                ->color('success')
+                ->icon('heroicon-o-truck'),
+            Stat::make('Balance', '$' . number_format($balance, 1, ',', '.'))
+                ->description($leyendaFechas)
+                ->color('success')
+                ->icon('heroicon-o-arrow-trending-up'),
+        ];
+    }
+}
